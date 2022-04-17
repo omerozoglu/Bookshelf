c3ccc7c0bcd7f88e9f595a49a35513e5a4aa53e9
omerozoglu <omerozoglu.1@gmail.com>
2022-04-17 17:43:56 +0300

    Add migration

---
+++ Bookshelf.API/Bookshelf.API.DataAccess/Concrete/BookshelfContext.cs
@@ -0,0 +1,16 @@
+﻿using Bookshelf.API.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookshelf.API.DataAccess.Concrete
+{
+    public class BookshelfContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=(localdb)\\MSSQLLocalDB; database=BookshelfApiDB; Integrated Security=true;");
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Book> Books { get; set; }
+    }
+}
