27d489ced889b8fbfb1b583506623245c250954a
rasenshuriken <omerozoglu.1@gmail.com>
2022-06-24 09:28:59 +0300

    MongoDB

---
+++ Bookshelf.API/Bookshelf.API.DataAccess/Concrete/MongoDB/Context/MongoDBContext.cs
@@ -0,0 +1,24 @@
+﻿using Bookshelf.API.Entities.Concrete;
+using Bookshelf.API.Entities.Concrete.Interfaces;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace Bookshelf.API.DataAccess.Concrete.MongoDB.Context
+{
+    public class MongoDBContext : IMongoLogDBContext
+    {
+        private IMongoDatabase _db { get; set; }
+        private MongoClient _mongoClient { get; set; }
+        public IClientSessionHandle Session { get; set; }
+        public MongoDBContext(IOptions<Mongosettings> configuration)
+        {
+            _mongoClient = new MongoClient(configuration.Value.Connection);
+            _db = _mongoClient.GetDatabase(configuration.Value.DatabaseName);
+        }
+
+        public IMongoCollection<Log> GetCollection<Book>(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
