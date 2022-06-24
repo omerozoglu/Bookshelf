b75531784c5642aa7fbfbf5228151546b14df618
rasenshuriken <omerozoglu.1@gmail.com>
2022-06-24 09:48:17 +0300

    MongoDB 2

---
+++ Bookshelf.API/Bookshelf.API.DataAccess/Concrete/MongoDB/Context/Mongosettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Bookshelf.API.DataAccess.Concrete.MongoDB.Context
+{
+    public class Mongosettings
+    {
+        public string Connection { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
---
+++ Bookshelf.API/Bookshelf.API.Entities/Interfaces/IMongoLogDBContext.cs
@@ -0,0 +1,9 @@
+﻿using MongoDB.Driver;
+
+namespace Bookshelf.API.Entities.Concrete.Interfaces
+{
+    public interface IMongoLogDBContext
+    {
+        IMongoCollection<Log> GetCollection<Book>(string name);
+    }
+}
