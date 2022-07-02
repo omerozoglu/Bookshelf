bd260921d314f1ca05398c6183fc14aa0d1f4ca7
rasenshuriken <omerozoglu.1@gmail.com>
2022-07-03 00:46:14 +0300

    Footer added

--- client/src/app/components/footer/footer.component.ts
+++ client/src/app/components/footer/footer.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+  title = 'Bookshelf';
   constructor() { }
 
   ngOnInit(): void {
